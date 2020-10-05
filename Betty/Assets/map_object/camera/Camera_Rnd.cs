8e4292cc6338e7a9c789eb810e8a918c04cdcda7
itoukent___ <kenkenrekoodo@gmail.com>
2020-10-05 23:27:25 +0900

    月曜日作業分

--- Betty/Assets/map_object/camera/Camera_Rnd.cs
+++ Betty/Assets/map_object/camera/Camera_Rnd.cs
@@ -7,16 +7,19 @@ public class Camera_Rnd : MonoBehaviour
     [SerializeField] private float camera_rot_time; //カメラの見る向きを変えるまでの時間
     [SerializeField] private float camera_dir;  //カメラの見る向き
     private float time;
+
+    private float deltatime;
+    private bool flg;
     // Start is called before the first frame update
     void Start()
     {
         switch (camera_dir)
         {
             case 1: //左向き
-                gameObject.transform.localEulerAngles = new Vector3(-90, 0, 0);
+                //gameObject.transform.localEulerAngles = new Vector3(270, 0, 0);
                 break;
             case 2: //右向き
-                gameObject.transform.localEulerAngles = new Vector3(-90, 180, 0);
+                //gameObject.transform.localEulerAngles = new Vector3(90, 180, 0);
                 break;
             case 3: //上向き
 
@@ -42,13 +45,16 @@ public class Camera_Rnd : MonoBehaviour
         switch (camera_dir)
         {
             case 1: //左向き
-                gameObject.transform.localEulerAngles = new Vector3(270, 0, 0);
+
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(90, 270, 0), 1f);
                 break;
             case 2: //右向き
-                gameObject.transform.localEulerAngles = new Vector3(90, 0, 0);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 270, 0), 1f);
+                //gameObject.transform.localEulerAngles = new Vector3(90, 0, 0);
                 break;
             case 3: //上向き
-                gameObject.transform.localEulerAngles = new Vector3(180, 0, 0);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(270, 270, 0), 1f);
+                //gameObject.transform.localEulerAngles = new Vector3(180, 0, 0);
                 break;
             case 4: //下向き
 
