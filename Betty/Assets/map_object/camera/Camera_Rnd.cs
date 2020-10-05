7a38172df1e2564524bbae08314d7da6a27645eb
itoukent <kenkenrekoodo@gmail.com>
2020-10-05 11:14:57 +0900

    金曜日進めた

---
+++ Betty/Assets/map_object/camera/Camera_Rnd.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Rnd : MonoBehaviour
+{
+    [SerializeField] private float camera_rot_time; //カメラの見る向きを変えるまでの時間
+    [SerializeField] private float camera_dir;  //カメラの見る向き
+    private float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (camera_dir)
+        {
+            case 1: //左向き
+                gameObject.transform.localEulerAngles = new Vector3(-90, 0, 0);
+                break;
+            case 2: //右向き
+                gameObject.transform.localEulerAngles = new Vector3(-90, 180, 0);
+                break;
+            case 3: //上向き
+
+                break;
+            case 4: //下向き
+
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if(time > camera_rot_time)
+        {
+            camera_dir = Random.Range(1, 4);
+            Debug.Log("一周");
+            time = 0;
+        }
+
+        switch (camera_dir)
+        {
+            case 1: //左向き
+                gameObject.transform.localEulerAngles = new Vector3(270, 0, 0);
+                break;
+            case 2: //右向き
+                gameObject.transform.localEulerAngles = new Vector3(90, 0, 0);
+                break;
+            case 3: //上向き
+                gameObject.transform.localEulerAngles = new Vector3(180, 0, 0);
+                break;
+            case 4: //下向き
+
+                break;
+        }
+    }
+}
--- Betty/Assets/map_object/camera/camera_rot.cs
+++ Betty/Assets/map_object/camera/camera_rot.cs
@@ -2,19 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class camera_rot : MonoBehaviour
+public class Camera_Rot : MonoBehaviour
 {
     private Vector3 Rot;
     public float RotationSpeed = 0;
+    [SerializeField] private int Rot_Dir;   //１＝ｘ、２＝ｙ、３＝ｚ
     void Start()
     {
         Rot = new Vector3(transform.rotation.x,transform.rotation.y,transform.rotation.z);
+        //switch (Rot_Dir)
+        //{
+        //    case 1:
+        //        transform.localEulerAngles = new Vector3(Rot.x + sin, Rot.y, Rot.z);
+        //        break;
+
+        //    case 2:
+        //        transform.localEulerAngles = new Vector3(Rot.x, Rot.y + sin, Rot.z);
+        //        break;
+
+        //    case 3:
+        //        transform.localEulerAngles = new Vector3(Rot.x, Rot.y, Rot.z + sin);
+        //        break;
+        //}
     }
     // Update is called once per frame
     void Update()
     {
+
         float sin = Mathf.Sin(Time.time) * RotationSpeed;
         //transform.rotation = Quaternion.Euler(Rot.x, Rot.y, Rot.z);
-        transform.localEulerAngles = new Vector3(Rot.x, Rot.y, Rot.z + sin);
+        //transform.localEulerAngles = new Vector3(Rot.x, Rot.y, Rot.z + sin);
+        switch (Rot_Dir)
+        {
+            case 1:
+                transform.localEulerAngles = new Vector3(Rot.x + sin, Rot.y, Rot.z);
+                break;
+
+            case 2:
+                transform.localEulerAngles = new Vector3(Rot.x, Rot.y + sin, Rot.z);
+                break;
+
+            case 3:
+                transform.localEulerAngles = new Vector3(Rot.x, Rot.y, Rot.z + sin);
+                break;
+        }
     }
 }
