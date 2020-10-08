9e9d7cb07c7b8b4803d65f6eb4d0be0833564e15
itoukent <kenkenrekoodo@gmail.com>
2020-10-08 16:17:39 +0900

    木曜日作業終了分、α提出済み

---
+++ Betty/Assets/Scripts/StageStatus.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageStatus : MonoBehaviour
+{
+    [SerializeField] private Transform Player_pos;
+    [SerializeField] private Material Wall_mat;
+    private Color color ;
+    private float Alphacolor = 1.0f;
+    private float ChangeAlphacolor = 0.2f;
+    private Vector3 Back_pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        color = Wall_mat.color;
+        Back_pos = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Player_pos.position.z > Back_pos.z)
+        {
+            color.a = ChangeAlphacolor;
+            Wall_mat.color = color;
+        }
+        else
+        {
+            color.a = Alphacolor;
+            Wall_mat.color = color;
+        }
+    }
+}
---
+++ Betty/Assets/map_object/camera/test.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //パーティクルを指定
+    private ParticleSystem particle;
+
+
+    private void Start()
+    {
+        particle = GetComponent<ParticleSystem>();
+        particle.Simulate(5.0f, true, true);
+        particle.Play();
+    }
+    //void Awake()
+    //{
+    //    StartCoroutine(testLoop(5f, () => {
+    //        particle.Simulate(2.0f, true, true);
+    //        particle.Play();
+    //    }));
+    //}
+
+    ////FOR TEST
+    //IEnumerator testLoop(float waitTme, UnityAction action)
+    //{
+    //    var waitTime = new WaitForSeconds(waitTme);
+    //    while (true)
+    //    {
+    //        yield return waitTime;
+    //        action();
+    //    }
+    //}
+}
