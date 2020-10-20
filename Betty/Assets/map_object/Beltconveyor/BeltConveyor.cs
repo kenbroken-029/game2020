e27c0fa6d85f30b8d7ef14749668c1fc22677b68
itoukent___ <kenkenrekoodo@gmail.com>
2020-10-20 12:07:18 +0900

    1020火曜日午前作業分

---
+++ Betty/Assets/map_object/Beltconveyor/BeltConveyor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltConveyor : MonoBehaviour
+{
+    [SerializeField] private float Speed;
+    [SerializeField] private Vector3 Direction;
+    [SerializeField] private float currentspeed { get { return _currentspeed; } }
+    [SerializeField] private float forcepower;
+    private float _currentspeed;
+    private List<Rigidbody> rigidbodies = new List<Rigidbody>();
+    [SerializeField] private bool moveflg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Direction = Direction.normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _currentspeed = moveflg ? Speed : 0;
+    }
+}
